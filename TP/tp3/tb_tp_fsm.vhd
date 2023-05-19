634e2ce29f7e76048edc8fcf47b66e81d4724530
thomas <angenault.thomas@gmail.com>
2023-05-19 12:27:24 +0200

    TP3

--- TP/tp3/tb_tp_fsm.vhd
+++ TP/tp3/tb_tp_fsm.vhd
@@ -6,31 +6,46 @@ end tb_tp_fsm;
 
 architecture behavioral of tb_tp_fsm is
 
-	signal resetn      : std_logic := '0';
-	signal clk         : std_logic := '0';
-	--a completer 
+	signal resetn          : std_logic := '0';
+	signal clk             : std_logic := '0';
+	signal s_btn_restart   : std_logic := '0';
+	signal s_led_r         : std_logic := '0';	
+    signal s_led_g         : std_logic := '0';	
+    signal s_led_b         : std_logic := '0';	
 	
 	-- Les constantes suivantes permette de definir la frequence de l'horloge 
 	constant hp : time := 5 ns;      --demi periode de 5ns
 	constant period : time := 2*hp;  --periode de 10ns, soit une frequence de 100Hz
 	
 	
-	component tp_fsm
-		port ( 
-			clk			: in std_logic; 
-			resetn		: in std_logic
-			--a completer
-		 );
-	end component;
-	
+	component tp_fsm is
+    generic (
+        counter_led_max    : positive
+    );
+    port ( 
+		clk			: in std_logic; 
+        resetn		: in std_logic;
+        btn_restart : in std_logic;
+        led_r       : out std_logic;
+        led_b       : out std_logic;
+        led_g       : out std_logic
+     );
+end component;
+
 	
 
 	begin
 	dut: tp_fsm
+	generic map(
+            counter_led_max  => 6
+            )
         port map (
             clk => clk, 
-            resetn => resetn
-			--a completer
+            resetn => resetn,
+            btn_restart => s_btn_restart,
+            led_r => s_led_r,
+            led_g => s_led_g,
+            led_b => s_led_b
         );
 		
 	--Simulation du signal d'horloge en continue
@@ -47,8 +62,24 @@ architecture behavioral of tb_tp_fsm is
 		resetn <= '1';
 		wait for period*10;    
 		resetn <= '0';
-	   
-		--a completer
+		
+	   -- observation du cycle d'initalisation et de 5 états successifs : allumage led rouge, puis bleu, puis verte, puis rouge
+	   --- puis bleu 
+	   -- changement un état allumé led : 10 * period, durée d'un état = 6, soit une durée de 6*10*period;
+	   -- observation donc 6*60*period. 
+		wait for period *360;
+		
+		-- observation du signal restart au milieu d'un état (wait 1 period)
+		wait for 10*period;
+		s_btn_restart <= '1';
+		wait for 4 * period;
+		s_btn_restart <= '0';
+		wait for 3*6*10*period;       -- observation de 3 états 
+		
+		-- test resetn 
+		resetn <= '1'; 
+		wait for 6*period;
+		resetn <= '0';
 	   
 	   
 		wait;
--- TP/tp3/tp_fsm.vhd
+++ TP/tp3/tp_fsm.vhd
@@ -7,43 +7,53 @@ use ieee.NUMERIC_STD.ALL;
 
 entity tp_fsm is
     generic (
-        constant counter_led_max    : positive :=50
+        counter_led_max    : positive :=6
     );
     port ( 
 		clk			    : in std_logic; 
         resetn		    : in std_logic;
-        end_led_counter : out std_logic
+        btn_restart : in std_logic;
+        led_r       : out std_logic;
+        led_b       : out std_logic;
+        led_g       : out std_logic
      );
 end tp_fsm;
-
-
-
 
 
 architecture behavioral of tp_fsm is
 
     component counter_unit is
     generic(
-        constant counter_max    : positive
+        counter_max    : positive 
     );
     port ( 
 		clk			: in std_logic; 
         resetn		: in std_logic;
         end_counter	: out std_logic
-        
      );
 end component;
     -- signaux pour le compteur de cycle de led
-    signal s_counter            : std_logic_vector (7 downto 0);
+    signal s_counter            : std_logic_vector (2 downto 0);
     signal s_end_led_counter    : std_logic := '0';
     signal s_end_counter        : std_logic := '0';
     
     
-    type state is (idle, state1, state2); --a modifier avec vos etats
+    type state is (idle, state_led_r, state_led_b, state_led_g); --a modifier avec vos etats
     
     signal current_state : state;  --etat dans lequel on se trouve actuellement
     signal next_state : state;	   --etat dans lequel on passera au prochain coup d'horloge
     
+    
+    -- signaux pour l'état de commande de led  (si le signal est à 1, alors la led clignote)
+    signal s_state_led_r : std_logic := '0';      
+    signal s_state_led_g : std_logic := '0';
+    signal s_state_led_b : std_logic := '0';
+    
+    signal s_led_on     : std_logic := '0';
+    
+    signal s_led_r       : std_logic := '0';
+    signal s_led_g       : std_logic := '0';
+    signal s_led_b       : std_logic := '0';
 
 	
 	
@@ -51,7 +61,7 @@ end component;
     
     count: counter_unit
     generic map(
-        counter_max => 200
+        counter_max => 200000000
     )
     port map(
         clk => clk,
@@ -60,77 +70,130 @@ end component;
     );
     
     
-        led_counter : process(clk, resetn)
+        led_counter : process(clk, resetn, btn_restart)
         begin
             if(resetn = '1') then
                 s_counter <= (others => '0');
-            elsif (rising_edge(clk)) then     
-                if (s_end_led_counter = '1') then
+             
+           
+            
+            elsif (rising_edge(clk)) then  
+                if ((s_end_led_counter = '1' and s_end_counter = '1') or btn_restart = '1')then
                     s_counter <= (others => '0');
-                else
-                    if(s_end_counter = '1') then
+                    
+                elsif(s_end_counter = '1') then
                         s_counter <= s_counter + 1;
-                    else
-                        s_counter <= s_counter;
-                    end if;
-                end if;  
-            end if;          
+                else
+                    s_counter <= s_counter;        
+                end if;
+            end if;   
         end process led_counter;
 	
 	
 	-- comparateur pour compteur de led
-	s_end_led_counter <= '1' when (s_counter = std_logic_vector(to_unsigned(counter_led_max,8)))
+	s_end_led_counter <= '1' when (s_counter = std_logic_vector(to_unsigned(counter_led_max - 1,8)))
 	else '0';
 	
-	-- récupération du signal de fin de comptage de led
-	end_led_counter <= s_end_led_counter;
-	
+
 	
 	--------------------------------------------------------------------------
 	-- state --
-		process(clk,resetn)
+		process(clk,resetn, btn_restart)
 		begin
-            if(resetn='1') then
-            
+            if(resetn = '1') then
                 current_state <= idle;
-                 
+                s_led_on <= '0';
+            
 			elsif(rising_edge(clk)) then
-			
-				current_state <= next_state;
-				
-				--a completer avec votre compteur de cycles
-				
-				
-            end if;
+			     current_state <= next_state;
+			     
+			     
+			     if(btn_restart = '1') then
+                    s_led_on <= '0';
+                    
+                 else 
+                     -- commande led allumée, pour les sychroniser
+                     if( s_end_counter = '1') then
+                         s_led_on <= not s_led_on;
+                     else
+                         s_led_on <= s_led_on;
+                     end if;
+                  end if;
+			    end if; 
 		end process;
 		
 		
+		s_led_r <= s_led_on and s_state_led_r;
+		s_led_g <= s_led_on and s_state_led_g;
+		s_led_b <= s_led_on and s_state_led_b;
+		
+		
+		
+		led_r <= s_led_r;
+		led_g <= s_led_g;
+		led_b <= s_led_b;
 		
 		
 --		-- FSM
---		process(current_state,XX) --a completer avec vos signaux
---		begin		
---           case current_state is
---              when idle =>
---				next_state <= state1; --prochain etat
+		process(current_state,s_end_led_counter, s_end_counter, btn_restart) --a completer avec vos signaux
+		begin		
+           case current_state is
+              when idle => 
+                    s_state_led_r <= '1';
+                    s_state_led_g <= '1';
+                    s_state_led_b <= '1';
+--				next_state <= 
+                    if(s_end_led_counter = '1' and s_end_counter = '1') then
+                        next_state <= state_led_r;
+                    else 
+                       next_state <= idle;
+                    end if;  --prochain etat
 				
 --                --signaux pilotes par la fsm
               
---              when state1 =>
---				next_state <= state1;
+              when state_led_r => 
+                   s_state_led_r <= '1';
+                   s_state_led_g <= '0';
+                   s_state_led_b <= '0';
+
+				     if(s_end_led_counter = '1' and s_end_counter = '1') then
+                        next_state <= state_led_b;
+                     elsif (btn_restart = '1') then
+                        next_state <= idle;
+                     else
+                        next_state <= state_led_r;
+                     end if;
+                     
+				when state_led_b => 
+                   s_state_led_r <= '0';
+                   s_state_led_g <= '0';
+                   s_state_led_b <= '1';
+
+				     if(s_end_led_counter = '1' and s_end_counter = '1') then
+                        next_state <= state_led_g;
+                     elsif (btn_restart = '1') then
+                        next_state <= idle;
+                     else
+                        next_state <= state_led_b;
+                     end if;
 				
---                --signaux pilotes par la fsm
-              
---              when state2 =>
---				next_state <= state1;
+             when state_led_g => 
+                   s_state_led_r <= '0';
+                   s_state_led_g <= '1';
+                   s_state_led_b <= '0';
+
+				     if(s_end_led_counter = '1' and s_end_counter = '1') then
+                        next_state <= state_led_r;
+                     elsif (btn_restart = '1') then
+                        next_state <= idle;
+                     else
+                        next_state <= state_led_g;
+                     end if;
 				
---                --signaux pilotes par la fsm
-              
-              
---              end case;
+              end case;
               
           
---		end process;
+		end process;
 		
 		
 		
